829bb3b0bcee39d18a17c8bc466a5cd2b572a3ab
PauMoriones <paumoga17@gmail.com>
2022-03-21 09:05:05 +0100

    Git

---
+++ app/src/main/java/cat/urv/deim/asm/exemple/QualificacionsActivity.kt
@@ -0,0 +1,13 @@
+package cat.urv.deim.asm.exemple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QualificacionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qualificacions)
+
+            //val: Int? = this.intent?.extra?.getInt("score")
+    }
+}
---
+++ app/src/main/java/cat/urv/deim/asm/exemple/QuestionsActivity.kt
@@ -0,0 +1,20 @@
+package cat.urv.deim.asm.exemple
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class QuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+
+        findViewById<Button>(R.id.questions_submit_button).setOnClickListener {
+            val intent: Intent = Intent()
+            intent.setClass(this,QuestionsActivity::class.java)
+            intent.putExtra("SCORE",5)
+            this.startActivity(intent)
+        }
+    }
+}
